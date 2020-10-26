d455ea63474446178937588cbe3c953fe2f4100e
00008918 <wiutstudent00008918@gmail.com>
2020-10-27 00:13:05 +0500

    Initial Commit

---
+++ Web-Site_Portfolio/scripts/script.js
@@ -0,0 +1,18 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');
+    const navLinks = document.querySelectorAll('.main-nav .nav-list li');
+
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle("main-nav-active")
+
+
+    burger.classList.toggle('toggle');
+    });
+
+   
+}
+
+navSlide();
+
